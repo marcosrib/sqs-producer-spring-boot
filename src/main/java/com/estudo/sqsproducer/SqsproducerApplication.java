0bec4babd980992332b900b08cf1337fe7fc7039
marcosrib <marcos.rib.sousa@gmail.com>
2022-02-24 21:39:00 -0300

    first commit

---
+++ src/main/java/com/estudo/sqsproducer/SqsproducerApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.sqsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsproducerApplication.class, args);
+	}
+
+}
